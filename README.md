cb9eaf88ce7bf9b339bd115bdee771961a59c918
Chang Dao Zheng (IA) <cdaozhen@dso.org.sg>
2023-05-26 12:19:37 +0800

    initial working server

--- README.md
+++ README.md
@@ -1,7 +1,30 @@
 # UDP client-server application
 
+
+## comunication between frontend and c++ application (analyse tradeoffs)
+C# interop marshalling 
+protobuf/grpc
+shared mem
+msg queue: mqtt, dds (paid) 
+TCP/IP socket + webassembly 
+RESTful service poco, boost 
+
+C# .NET core / razor
+
+copy constructors
+move constructors 
+rvalue refs
+
 ## User Instructions 
+```
 $ git clone https://github.com/changdaozheng/udp_messaging.git
 $ cd udp_messaging
+$ mkdir build
 $ cmake --build build
 $ build/server_client
+```
+
+
+a=c=0 
+
+c=4
--- src/utils.cpp
+++ src/utils.cpp
@@ -1,13 +1,14 @@
 #include "utils.h"
 
-std::string getCurrTime(){
+std::string getCurrTime()
+{
     // Get the current time
     std::chrono::system_clock::time_point now = std::chrono::system_clock::now();
     std::time_t currentTime = std::chrono::system_clock::to_time_t(now);
 
     // Convert the current time to a string
     std::stringstream ss;
-    ss << std::put_time(std::localtime(&currentTime), "%Y-%m-%d_%H:%M:%S");
+    ss << std::put_time(std::localtime(&currentTime), "%Y%m%d_%H%M%S");
     std::string currentTimeStr = ss.str();
 
     return currentTimeStr;

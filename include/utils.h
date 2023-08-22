ac7ed81014233549b071834c4165164cfc506ac7
LokQiJun <lokqj36@gmail.com>
2023-08-22 19:31:07 +0700

    Update utils.h

--- include/utils.h
+++ include/utils.h
@@ -5,13 +5,6 @@
 #include <iomanip> 
 #include <sstream>
 
-#ifndef CLIENT_PORT
-#define CLIENT_PORT 5000
-#endif
-#ifdef SERVER_PORT
-#define SERVER_PORT 5005
-#endif
-
 // Application constants
 #define PACKET_SIZE 1472
 
